ce8db06bd105c63be59b3c0760d867f5f25f574b
Ferdinand <ndimbecha@gmail.com>
2021-09-02 15:57:58 +0200

    Create an Express server

---
+++ expressServer.js
@@ -0,0 +1,34 @@
+import express from 'express'
+
+
+const app = express()
+
+let initNum = 0 
+app.get('/', (req,resp)=>{		
+	resp.send(initNum.toString())
+});
+
+app.post('/inc', (req,resp)=>{
+	initNum = initNum +1	
+	resp.send(initNum.toString())
+});
+
+
+app.post('/dec', (req,resp)=>{
+	initNum = initNum -1	
+	resp.send("I am decrementing")
+});
+
+app.post('/die', (req,resp)=>{
+		
+	resp.send("You have shutdown the server!")
+	expressServer.close() 
+});
+
+
+
+const port = 3008
+
+const expressServer = app.listen(port, ()=>{
+	console.log("The app is listening on http://localhost:" + port)
+})
